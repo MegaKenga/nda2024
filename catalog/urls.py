de00425822c12444cf5fd3330659aa7bc403475b
mega-kenga <marka.i09@gmail.com>
2023-12-18 00:17:11 +0300

    problem with def get_category_by_brand

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('brands/<brand_id>/', views.get_brand, name='brand'),
     path('categories/<category_id>/', views.get_category, name='category'),
-    path('products/<product_id>/', views.get_product, name='product'),
+    path('brands/<brand_id>/<category_id>/', views.get_category_by_brand, name='categories_by_brand'),
 ]
