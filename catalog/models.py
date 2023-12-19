13fe30da614a7d3fdc6e788d13008df6fbbc5eb7
MegaKenga <marka.i09@gmail.com>
2023-12-19 23:25:16 +0300

    models refactored

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 
+"""Общие классы и миксины"""
 class NotHidden(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_active=True)
@@ -9,6 +10,7 @@ class NotHidden(models.Manager):
 
 class DataModelsMixin(models.Model):
     description = models.TextField(default='', verbose_name='Описание')
+    place = models.IntegerField(blank=True, null=True, verbose_name='Место в списке')
     is_active = models.BooleanField(default=True, verbose_name='Активная категория')
 
     objects = models.Manager()
@@ -18,10 +20,14 @@ class DataModelsMixin(models.Model):
         abstract = True
 
 
+"""Модели"""
 class Brand(DataModelsMixin):
-    name = models.CharField(max_length=256, unique=True, verbose_name='Бренд')
+    name = models.CharField(max_length=128, verbose_name='Бренд')
+    # image = models.ImageField(upload_to='catalog/images/image', blank=True, null=True, verbose_name='Изображение')
+    # banner = models.ImageField(upload_to='catalog/images/banner', blank=True, null=True, verbose_name='Баннер')
 
     class Meta:
+        ordering = ['place']
         verbose_name = 'Бренд'
         verbose_name_plural = 'Бренды'
 
@@ -30,7 +36,13 @@ class Brand(DataModelsMixin):
 
 
 class Category(DataModelsMixin, MPTTModel):
-    name = models.CharField(max_length=512, verbose_name='Категория')
+    name = models.CharField(max_length=128, verbose_name='Имя группы')
+    brand = models.ForeignKey(
+        Brand,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='ID Бренда, к которому относится группа'
+    )
     parent = TreeForeignKey(
         'self',
         related_name='children',
@@ -39,11 +51,11 @@ class Category(DataModelsMixin, MPTTModel):
         blank=True,
         verbose_name='ID родительской категории'
     )
-
-    class MPTTMeta:
-        order_insertion_by = ['name']
+    # image = models.ImageField(upload_to='catalog/images/image', blank=True, null=True, verbose_name='Изображение')
+    # banner = models.ImageField(upload_to='catalog/images/banner', blank=True, null=True, verbose_name='Баннер')
 
     class Meta:
+        ordering = ['place']
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
@@ -52,7 +64,7 @@ class Category(DataModelsMixin, MPTTModel):
 
 
 class Product(DataModelsMixin):
-    name = models.CharField(max_length=512, unique=True, verbose_name='Товар')
+    name = models.CharField(max_length=50, unique=True, verbose_name='Товар')
     brand = models.ForeignKey(
         Brand,
         on_delete=models.SET_NULL,
@@ -61,12 +73,15 @@ class Product(DataModelsMixin):
     )
     category = models.ForeignKey(
         'Category',
-        on_delete=models.SET_NULL,
         null=True,
-        verbose_name='ID категории, к которой относится товар'
+        related_name='ancestor_for_product',
+        on_delete=models.SET_NULL,
+        verbose_name='Уникальный ID группы, к которой принадлежит товар'
     )
+    # image = models.ImageField(upload_to='catalog/images/image', blank=True, null=True, verbose_name='Изображение')
 
     class Meta:
+        ordering = ['place']
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
 
