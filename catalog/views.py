5a974363fc8352d7157e90e3d4a368992d621bb3
mega-kenga <marka.i09@gmail.com>
2023-12-24 02:01:26 +0300

    offer templates added, minor views and admin changes

--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,7 @@ from catalog.models import Category, Brand, Product, Offer
 
 def index(request):
     brands = Brand.objects.all().order_by('name')
-    categories = Category.objects.filter(parent=None).order_by('name')
+    categories = Category.objects.filter(parent=None).order_by('name') & Category.objects.filter(brand=None)
     context = {'brands': brands, 'categories': categories}
     return render(request, 'catalog/index.html', context=context)
 
@@ -26,8 +26,29 @@ def get_category(request, category_id):
     context = {'category': category, 'parent': parent, 'children': children, 'products': products}
     return render(request, 'catalog/category.html', context=context)
 
-def get_offer(request, category_id, product_id):
+
+def get_offer_by_category(request, category_id, product_id):
     items = Offer.objects.filter(product=product_id)
-    category_id = Product.objects.get(categories=category_id)
-    context = {'items': items, 'category_id': category_id}
-    return render(request, 'catalog/offer.html', context=context)
+    category = Product.objects.filter(categories=category_id)
+    context = {'items': items, 'category_id': category}
+    return render(request, 'catalog/offer_by_category.html', context=context)
+
+
+def get_category_by_brand(request, brand_id, category_id):
+    brand = Brand.objects.get(pk=brand_id)
+    category = Category.objects.get(pk=category_id)
+    parent = category.parent
+    if parent is None:
+        parent = None
+    children = Category.objects.filter(brand=brand_id) & Category.objects.filter(parent=category_id)
+    products = Product.objects.filter(categories=category.id)
+    context = {'brand': brand, 'category': category, 'parent': parent, 'children': children, 'products': products}
+    return render(request, 'catalog/category_by_brand.html', context=context)
+
+
+def get_offer_by_brand(request, brand_id, category_id, product_id):
+    offers = Offer.objects.filter(product=product_id)
+    brand = Brand.objects.get(pk=brand_id)
+    product = Product.objects.get(categories=category_id)
+    context = {'offers': offers, 'product_id': product, 'brand_id': brand}
+    return render(request, 'catalog/offer_by_category.html', context=context)
