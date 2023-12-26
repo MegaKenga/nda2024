d3aef0a04b8b71a90f83df76c02ce64bf6c84ba7
Vladimir Kromm <vladimir.kromm@mercedes-benz.com>
2023-12-26 08:22:03 +0100

    fixed two small issues

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('categories/<category_id>', views.get_category, name='category'),
     # path('categories/<category_id>/<product_id>', views.get_offer_by_category, name='offer_by_category'),
     # path('brands/<brand_id>/<category_id>/', views.get_category_by_brand, name='category_by_brand'),
-    path('/<brand_id>/<product_id>', views.get_offer, name='offer'),
+    path('<brand_id>/<product_id>', views.get_offer, name='offer'),
 ]
