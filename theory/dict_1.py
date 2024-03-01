9fa3a8523c4096a2b147eaf38dba09d2e3241b16
Vladimir Kromm <vladimir.kromm@mercedes-benz.com>
2024-03-01 17:50:50 +0100

    dict review

--- theory/dict_1.py
+++ theory/dict_1.py
@@ -44,28 +44,55 @@ from collections import Counter
 def data_list(data):
     d_list = []
     for login, info in data.items():
+        # ОК, мы имеем кортеж
         login_info = ('login', login)
         for x in info.items():
+            # Тут мы вставляем этот кортеж в наш массив, но зачем-то делаем это в цикле?
+            # Те этот кортеж будет вставлен несколько раз, смотря сколько осталось items в info.
             d_list.append(login_info)
+            # теперь вставляем кортежи ("firstName", ""Vladimir") ("lastName", "Petrov")
             d_list.append(x)
-        print(dict(d_list))
-
+        # тут мы имеем массив с кортежами, причем ('login', 'vlad_pe') дважды,;
+        # [('login', 'vlad_pe'), ('firstName', 'Vladimir'), ('login', 'vlad_pe'), ('secondName', 'Petrov')
+    # далее мы добавляем по 4 элемента (имя, фамилия и логин дважды) для каждого data.items()
+    # да, ты можешь из такого списка кортежей сделать словарь методом dict(), но левый элемент кортежа (ключ) дублируется и в итоге из всех "людей"
+    # в итоговом словаре будет только один логин, только одно имя и только одна фамилия,
+    # print(dict(d_list))
 
 data_list(data)
+
+# опция первая (лист компрехеншн)
+def format_data_to_list1():
+    return [{"login": key, **value} for key, value in data.items()]
+
+# то же самое через цикл for
+def format_data_to_list2():
+    persons = []
+    for key, value in data.items():
+        persons.append({"login": key, **value})
+    return persons
+
 
 
 def second_name(data):
     second_names = []
+    # отлично! Единственное замечание имя переменной second_name, в этой переменное лежит что-то другое.
+    # Я бы назвал ее person_data например
     for second_name in data.values():
         second_names.append(second_name['secondName'])
     print(list(set(second_names)))
 
+# сможешь эту же задачу решить через "лист компрехенш" в одну строчку как format_data_to_list1?
 
-second_name(data)
+# second_name(data)
 
 
 def names_list(data):
     names = []
+    # задача решена, это хорошо. Лично я бы решил чуток по другому:
+    # 1. переименовал переменную first_names которая хранит что-то другое
+    # 2. постарался бы не испоьлзовать сторонних библиотек и импортов без особой неоходимости
+    # это "вкусовщина" но ниже мое решение, просто для примера.
     for first_names in data.values():
         name = first_names.get('firstName')
         if name is not None:
@@ -74,4 +101,17 @@ def names_list(data):
     print(dict(x))
 
 
-names_list(data)
+# names_list(data)
+
+
+def name_list2():
+    output = {}
+    for person in data.values():
+        first_name = person.get('firstName', None)
+        if first_name is None:
+            continue
+        current_name_count = output.get(first_name, 0)
+        output[first_name] = current_name_count + 1
+    return output
+
+name_list2()
