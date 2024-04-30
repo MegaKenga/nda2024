d757be2c69f9873aeb88b64333cb538a5872bf10
Vladimir Kromm <vladimir.kromm@mercedes-benz.com>
2024-04-30 17:34:46 +0200

    some regex test cases

---
+++ nda_email/email_regex_test.py
@@ -0,0 +1,32 @@
+
+import unittest
+import re
+
+EMAIL_PATTERN = r'([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)'
+# todo: write more test cases, improve pattern
+
+
+class TestEmailRegex(unittest.TestCase):
+    def setUp(self):
+        self.regex = re.compile(EMAIL_PATTERN)
+
+    def test_valid_emails(self):
+        valid_emails = [
+            "a@b.com",
+            "a.b@c.d.ru",
+            " a.b@c.d.ru ",
+        ]
+        for phone in valid_emails:
+            with self.subTest(phone=phone):
+                self.assertIsNotNone(self.regex.match(phone))
+
+    def test_invalid_phones(self):
+        invalid_emails = [
+            "a@b",
+            "a@b.c",
+            "abc",
+            "a@b@c",
+        ]
+        for phone in invalid_emails:
+            with self.subTest(phone=phone):
+                self.assertIsNone(self.regex.match(phone))
