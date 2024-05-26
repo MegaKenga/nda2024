545d2f774cb9c436253f61e82791d1781b6abc80
mega-kenga <marka.i09@gmail.com>
2024-05-27 01:08:15 +0300

    captcha finished

---
+++ nda_email/captcha.py
@@ -0,0 +1,35 @@
+import json
+import sys
+import requests
+
+from django.conf import settings
+
+
+def get_client_ip(request):
+    """получение IP пользователя"""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR', '')
+    return ip
+
+
+def yandex_captcha_validation(token, client_ip):
+    if len(token) == 0:
+        return False
+    resp = requests.get(
+        "https://captcha-api.yandex.ru/validate",
+        {
+            "secret": settings.YACAPTCHA_SERVER,
+            "token": token,
+            "ip": client_ip
+        },
+        timeout=1
+    )
+    server_output = resp.content.decode()
+    print(server_output)
+    if resp.status_code != 200:
+        print(f"Allow access due to an error: code={resp.status_code}; message={server_output}", file=sys.stderr)
+        return True
+    return json.loads(server_output)["status"] == "ok"
