2460568248fd5a9579774d77a1e335ab4ead400f
mega-kenga <marka.i09@gmail.com>
2024-06-02 02:43:27 +0300

    core models added

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from core.models import MainPage
+from catalog.admin_filters import DropdownFilter
 
-# Register your models here.
+
+class MainPageAdmin(admin.ModelAdmin):
+    list_display = (
+        'advert_name',
+        'advert_category',
+        'advert_text',
+        'advert_image',
+        'status'
+    )
+    list_editable = ('advert_text', )
+    list_filter = ('advert_name', ('advert_category', DropdownFilter), 'status')
+    fields = [
+        'advert_name',
+        'advert_category',
+        'advert_text',
+        'advert_image',
+        'status'
+    ]
+    view_on_site = True
+    actions_on_bottom = True
+    list_per_page = 25
+    search_fields = ['advert_name']
+
+
+admin.site.register(MainPage, MainPageAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from catalog.models import Category
 
-# Create your models here.
+
+class MainPage(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DRAFT', 'Черновик'
+        PUBLISHED = 'PUBLISHED', 'Активен'
+        ARCHIVED = 'ARCHIVED', 'В архиве'
+
+    advert_name = models.CharField(
+        max_length=128,
+        null=False,
+        blank=False,
+        verbose_name='Название рекламного блока'
+    )
+    advert_category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        verbose_name='Категория для рекламы на главной странице'
+    )
+    advert_text = models.CharField(
+        max_length=256,
+        null=False,
+        blank=False,
+        verbose_name='Текст рекламного блока'
+    )
+    advert_image = models.ImageField(
+        upload_to='main_page/banner',
+        default='',
+        null=False,
+        blank=False,
+        verbose_name='Изображение для рекламного блока'
+    )
+    status = models.CharField(
+        choices=Status.choices,
+        default=Status.DRAFT,
+        verbose_name='Статус показа на страницах'
+    )
+
+    class Meta:
+        verbose_name = 'Реклама на главной странице'
+        verbose_name_plural = 'Реклама на главной странице'
+
+    def __str__(self):
+        return str(self.advert_name)
