c7aef644c7ee2372ae96b7e5908b6d09ca59258a
mega_kenga <marka.i09@gmail.com>
2025-05-11 21:52:57 +0300

    добавил exception в import_from_excel

--- files/views.py
+++ files/views.py
@@ -6,25 +6,29 @@ from catalog.models import Offer, Product
 
 def import_from_excel(request, product_id):
     if request.method == 'POST':
-        excel_file = request.FILES['excel_file']
-        wb = load_workbook(excel_file)
-        ws = wb.active
+        try:
+            excel_file = request.FILES['excel_file']
+            wb = load_workbook(excel_file)
+            ws = wb.active
 
-        for row in ws.iter_rows(min_row=2, values_only=True):
-            if row[0] is not None:
-                name, text_description, shipping_pack, place = row
-                product_id = int(product_id)
-                Offer.objects.create(
-                    name=name,
-                    text_description=text_description,
-                    shipping_pack=shipping_pack,
-                    place=place,
-                    status='PUBLISHED',
-                    product_id=product_id
-                )
-            else:
-                break
-        product = get_object_or_404(Product, pk=int(product_id))
-        return redirect('offer', product_slug= product.slug, brand_slug=product.brand.slug)
+            for row in ws.iter_rows(min_row=2, values_only=True):
+                if row[0] is not None:
+                    name, text_description, shipping_pack, place = row
+                    product_id = int(product_id)
+                    Offer.objects.create(
+                        name=name,
+                        text_description=text_description,
+                        shipping_pack=shipping_pack,
+                        place=place,
+                        status='PUBLISHED',
+                        product_id=product_id
+                    )
+                else:
+                    break
+            product = get_object_or_404(Product, pk=int(product_id))
+            return redirect('offer', product_slug=product.slug, brand_slug=product.brand.slug)
+        except Exception:
+            product = get_object_or_404(Product, pk=int(product_id))
+            return redirect('offer', product_slug=product.slug, brand_slug=product.brand.slug)
 
     return render(request, 'core/components/excel_input.html')
