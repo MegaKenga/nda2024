071f9834151ab5042537d906b058af10aa5320d8
Ivan Markarov <marka.i09@gmail.com>
2026-01-12 14:00:29 +0300

    поправил / в url адресах каталога

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,9 +4,9 @@ from catalog import views
 
 urlpatterns = [
 
-    path('brands/<slug:brand_slug>', views.BrandView.as_view(), name='brand'),
-    path('categories/<slug:category_slug>', views.CategoryView.as_view(), name='category'),
-    path('<slug:brand_slug>/<slug:product_slug>', views.OfferView.as_view(), name='offer'),
+    path('brands/<slug:brand_slug>/', views.BrandView.as_view(), name='brand'),
+    path('categories/<slug:category_slug>/', views.CategoryView.as_view(), name='category'),
+    path('<slug:brand_slug>/<slug:product_slug>/', views.OfferView.as_view(), name='offer'),
     path('search/', views.SiteSearchView.as_view(), name='search_page'),
     path('duplicates/', views.DuplicatesView.as_view(), name='duplicates'),
 ]
