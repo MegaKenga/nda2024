b0b2d3f93764016d8a6eb1e12c68dfcaf503d303
mega_kenga <marka.i09@gmail.com>
2026-08-14 23:08:09 +0300

    вырезал webp optimizer

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django.urls import path, reverse
 
 from core.models import MainPageInfoBlock
-from core import media_optimizer_views
 from catalog.admin_filters import RelatedOnlyDropdownFilter
 
 
@@ -35,70 +33,3 @@ class MainPageInfoBlockAdmin(admin.ModelAdmin):
 
 
 admin.site.register(MainPageInfoBlock, MainPageInfoBlockAdmin)
-
-"""Админка оптимизатора медиа файлов"""
-
-_original_get_urls = admin.site.get_urls
-_original_get_app_list = admin.site.get_app_list
-
-
-def _optimizer_admin_urls():
-    wrap = admin.site.admin_view
-    return [
-        path(
-            'media-optimizer/',
-            wrap(media_optimizer_views.media_optimizer_admin_page),
-            name='core_media_optimizer',
-        ),
-        path(
-            'media-optimizer/start/',
-            wrap(media_optimizer_views.media_optimizer_start),
-            name='core_media_optimizer_start',
-        ),
-        path(
-            'media-optimizer/stop/',
-            wrap(media_optimizer_views.media_optimizer_stop),
-            name='core_media_optimizer_stop',
-        ),
-        path(
-            'media-optimizer/reset/',
-            wrap(media_optimizer_views.media_optimizer_reset),
-            name='core_media_optimizer_reset',
-        ),
-        path(
-            'media-optimizer/status/',
-            wrap(media_optimizer_views.media_optimizer_status),
-            name='core_media_optimizer_status',
-        ),
-        path(
-            'media-optimizer/report/',
-            wrap(media_optimizer_views.media_optimizer_report),
-            name='core_media_optimizer_report',
-        ),
-    ]
-
-
-def custom_get_urls():
-    return _optimizer_admin_urls() + _original_get_urls()
-
-
-def custom_get_app_list(request, app_label=None):
-    app_list = _original_get_app_list(request, app_label=app_label)
-    if request.user.is_staff:
-        app_list.append({
-            'name': 'Инструменты',
-            'app_label': 'tools',
-            'app_url': reverse('admin:core_media_optimizer'),
-            'has_module_perms': True,
-            'models': [{
-                'name': 'Оптимизация изображений',
-                'object_name': 'MediaOptimizer',
-                'admin_url': reverse('admin:core_media_optimizer'),
-                'view_only': True,
-            }],
-        })
-    return app_list
-
-
-admin.site.get_urls = custom_get_urls
-admin.site.get_app_list = custom_get_app_list
--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
 
-from core import media_optimizer_views
 
 urlpatterns = [
-    path('', media_optimizer_views.media_optimizer_page, name='media_optimizer'),
-    path('start/', media_optimizer_views.media_optimizer_start, name='media_optimizer_start'),
-    path('stop/', media_optimizer_views.media_optimizer_stop, name='media_optimizer_stop'),
-    path('reset/', media_optimizer_views.media_optimizer_reset, name='media_optimizer_reset'),
-    path('status/', media_optimizer_views.media_optimizer_status, name='media_optimizer_status'),
-    path('report/', media_optimizer_views.media_optimizer_report, name='media_optimizer_report'),
+
 ]
